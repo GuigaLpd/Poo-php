fc113b546882c6caaaa14d88674e440383f5e83a
GuigaLpd <guilhermecampos0108@gmail.com>
2025-10-16 22:41:17 -0300

    a

--- src/controller/MatriculaController.php
+++ src/controller/MatriculaController.php
@@ -1,3 +1,10 @@
 <?php
 
+include_once '../model/Aluno.php';
+
+
 echo "Matriculas Controller ta rodando irmao";
+  $aluno1 = new Aluno("123456");
+  $aluno2 = new Aluno("porra");
+
+  echo $aluno1;
--- src/model/Aluno.php
+++ src/model/Aluno.php
@@ -3,7 +3,14 @@
 class Aluno{
     private $rm;
 
+        public function __construct($rm){
+        $this->rm = $rm;
+    }
+
     public function getRm(): string{
         return $this->rm;
     }
+     public function setRm(): string{
+        $this->rm 
+}
 }
